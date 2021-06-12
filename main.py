ae8c04948a2cafab79a610f0614b03f1ebb925ae
lahindu <lahindu@gmail.com>
2021-06-12 16:04:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
-hostName = "8080"
+hostName = "0.0.0.0"
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes("<html><body><h1>Hi...</h1></body></html>", "utf-8"))
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+server_object = HTTPServer(server_address=(hostName, serverPort), RequestHandlerClass=MyServer)
+print("Server started http://%s:%s" % (hostName, serverPort))
+server_object.serve_forever()
